18e703c8c88b7fa8317b91036449315ec33ed203
Bianca <bianca.rocha1711@gmail.com>
2019-02-17 13:47:21 -0300

    changing prices on state with mutations

--- src/store/store.js
+++ src/store/store.js
@@ -21,5 +21,12 @@ export const store = new Vuex.Store({
       });
       return saleProducts;
     }
+  },
+  mutations: {
+    reducePrice: state => {
+      state.products.forEach(product => {
+        product.price -= 1;
+      });
+    }
   }
 });
